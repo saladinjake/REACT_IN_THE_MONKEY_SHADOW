e210c627de26ea8cc5b7556c1fcc9b8c5ae1c35b
saladinjake <juwavictor@gmail.com>
2022-06-14 18:16:57 +0100

    folderstructure sample for backend modules

--- backend-service/src/app.module.ts
+++ backend-service/src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI),
-    SharedModule, 
-    AuthModule
+   
+   MongooseModule.forRoot(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    bufferMaxEntries: 0,
+    bufferCommands: false
+  }),
+ 
   ],
   controllers: [AppController],
   providers: [AppService],
